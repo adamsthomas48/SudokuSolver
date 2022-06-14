7ca485ccb9005909b0e7a32e63f6edae5ce69112
Adam Thomas <adam@Adams-MacBook-Pro.local>
2022-06-14 01:07:53 -0600

    creating puzzle

--- sudokuSolver/src/main/kotlin/Puzzle.kt
+++ sudokuSolver/src/main/kotlin/Puzzle.kt
@@ -1,4 +1,15 @@
 class Puzzle(val n: Int, val cells: List<List<Cell>>) {
 
+    fun printAttributes(){
+        println("n: $n")
+        //print value of cells
+        for (row in cells) {
+            for (cell in row) {
+                print("${cell.value} ")
+            }
+            println()
+        }
+    }
+
 
 }
--- sudokuSolver/src/main/kotlin/Solver.kt
+++ sudokuSolver/src/main/kotlin/Solver.kt
@@ -2,19 +2,28 @@ import java.io.File
 
 class Solver(val inputFileName: String) {
     val possibleValues = mutableListOf<String>()
+    val file = File(inputFileName)
+    val n = file.readLines()[0].toInt()
+    val cells = createCellList()
+    val puzzle: Puzzle = Puzzle(n, cells)
 
-    fun createCellList(): List<List<Cell>> {
-        val cells = mutableListOf<List<Cell>>()
-        val n: Int
-        //create file from inputFile
-        val file = File(inputFileName)
-        n = file.readLines()[0].toInt()
-        //add any non whitespace characters from second line to possibleValues
+    init {
+        setPossibleValues()
+        puzzle.printAttributes()
+    }
+
+
+    fun setPossibleValues() {
         file.readLines()[1].forEach {
             if (it != ' ') {
                 possibleValues.add(it.toString())
             }
         }
+    }
+
+    fun createCellList(): List<List<Cell>> {
+        val cells = mutableListOf<List<Cell>>()
+        val n: Int
 
         //read each character from row 2 onwards and create a cell for each
         for (i in 2 until file.readLines().size) {
@@ -22,16 +31,10 @@ class Solver(val inputFileName: String) {
             file.readLines()[i].forEach {
                 if(it != ' ') {
                     row.add(Cell(it.toString()))
-                    print(it + " ")
                 }
-
-
             }
             cells.add(row)
-            println()
         }
-
-
 
         return cells
     }
